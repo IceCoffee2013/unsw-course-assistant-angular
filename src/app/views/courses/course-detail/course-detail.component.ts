4c55c19b68fea11879491f36af2c5a6b3dfb35ac
langley <295046974@qq.com>
2017-08-13 01:31:16 +1000

    course list, paginator

---
+++ src/app/views/courses/course-detail/course-detail.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute} from "@angular/router";
+import {Course} from "../../../models/course/course-model";
+import {CourseDetailService} from "../../../services/course/course-detail.service";
+
+
+@Component({
+  selector: 'app-course-detail',
+  templateUrl: 'course-detail.component.html',
+  styleUrls: ['course-detail.component.scss']
+})
+export class CourseDetailComponent implements OnInit {
+  public course: Course = new Course();
+
+  constructor(public courseDetailService: CourseDetailService,
+              public activeRoute: ActivatedRoute) {
+    console.log(this.courseDetailService);
+  }
+
+  ngOnInit() {
+    this.activeRoute.params.subscribe(
+      params => this.getCourse(params["id"])
+    );
+  }
+
+  public getCourse(id: number) {
+    this.courseDetailService
+      .getCourse(id)
+      .subscribe(
+        data => this.course = data,
+        error => console.error(error)
+      );
+  }
+}
