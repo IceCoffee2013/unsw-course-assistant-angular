857e9a029eabeb60b6eaada3f893b5aa95c7c33e
unswddk <303162101@qq.com>
2017-09-06 22:27:19 +1000

    Merge branch 'master' of https://github.com/IceCoffee2013/unsw-course-assistant-angular
    # Conflicts:
    #	src/app/app.module.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -48,11 +48,6 @@ export const rootRouterConfig: Routes = [
         data: { title: 'Experience', breadcrumb: 'EXPERIENCE'}
       },
       {
-        path: 'answer',
-        loadChildren: './views/top-answer/top-answer.module#TopAnswerModule',
-        data: { title: 'Answer', breadcrumb: 'TOP-ANSWER'}
-      },
-      {
         path: 'user',
         loadChildren: './views/user/user.module#UserModule',
         data: { title: 'UserProfile', breadcrumb: 'USER-PROFIlE'}
